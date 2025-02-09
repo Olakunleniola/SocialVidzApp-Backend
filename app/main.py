9bcd101f7d450f441a5a95c6d089e49f31179a73
devlakunle <devlakunle@gmail.com>
2025-02-09 18:48:59 +0100

    fixed mongo database issue

--- app/main.py
+++ app/main.py
@@ -6,6 +6,8 @@ from app.core.error_handlers import validation_exception_handler, httpx_timeout_
 from fastapi.exceptions import RequestValidationError
 import httpx
 from app.core.config import settings
+from motor.motor_asyncio import AsyncIOMotorClient
+
 
 
 
@@ -19,14 +21,23 @@ app.add_exception_handler(RequestValidationError, validation_exception_handler)
 app.add_exception_handler(httpx.ConnectTimeout, httpx_timeout_handler)
 app.add_exception_handler(ValueError, value_error_handler)
 app.add_exception_handler(HTTPException, httpexceptions_handler)
-app.add_exception_handler(Exception, exception_handler) 
+app.add_exception_handler(Exception, exception_handler)
 
-if settings.DEBUG:
-    from app.models.database import engine
-    @app.on_event('startup')
-    async def initailize_database():
+
+@app.on_event('startup')
+async def initailize_database():
+    if settings.DEBUG:
+        from app.models.database import engine
         async with engine.begin() as conn:
             await conn.run_sync(VideoDownload.metadata.create_all)
+    else:
+        global mongo_client
+        mongo_client = AsyncIOMotorClient(settings.DATABASE_URL)
+        app.state.mongo_db = mongo_client.get_database(settings.MONGO_DB_NAME)
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    mongo_client.close()
 
 # Include routes
 app.include_router(video_router, prefix="/social_vidz/api")
